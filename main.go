94bb6555e560487fed457a546cc708aadf73408d
Senthil Kumaran <senthilx@amazon.com>
2024-11-13 02:41:47 +0000

    updated with example.

--- main.go
+++ main.go
@@ -1,25 +1,64 @@
 package main
 
 import (
-  "fmt"
+	"context"
+	"log"
+	"time"
+
+	"eni-project/internal/ec2"
+	"github.com/aws/aws-sdk-go-v2/config"
+	awsec2 "github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
 func main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-  // to see how GoLand suggests fixing it.
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	// Load AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config: %v", err)
+	}
 
-  for i := 1; i <= 5; i++ {
-	//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session, 
-	// right-click your code in the editor and select the <b>Debug</b> option. 
-	fmt.Println("i =", 100/i)
-  }
+	// Create EC2 client
+	client := awsec2.NewFromConfig(cfg)
+
+	// Create ENI manager
+	eniManager := ec2.NewENIManager(client)
+
+	subnetID := "subnet-0a7bd03887dc3cbd5"
+	instanceID := "i-04890aa7cd8cf81f3"
+
+	// Create context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	// Create ENI
+	eni, err := eniManager.CreateENI(ctx, subnetID)
+	if err != nil {
+		log.Fatalf("Failed to create ENI: %v", err)
+	}
+	log.Printf("Created ENI: %s\n", *eni.NetworkInterface.NetworkInterfaceId)
+
+	// Attach ENI
+	attachID, err := eniManager.AttachENI(ctx, *eni.NetworkInterface.NetworkInterfaceId, instanceID)
+	if err != nil {
+		log.Fatalf("Failed to attach ENI: %v", err)
+	}
+	log.Printf("Attached ENI with attachment ID: %s\n", *attachID)
+
+	// Wait a bit before detaching (in a real application, you might wait until you're done using it)
+	time.Sleep(5 * time.Second)
+
+	// Detach ENI
+	if err := eniManager.DetachENI(ctx, *attachID); err != nil {
+		log.Fatalf("Failed to detach ENI: %v", err)
+	}
+	log.Println("Detached ENI")
+
+	// Wait for detachment to complete
+	time.Sleep(5 * time.Second)
+
+	// Delete ENI
+	if err := eniManager.DeleteENI(ctx, *eni.NetworkInterface.NetworkInterfaceId); err != nil {
+		log.Fatalf("Failed to delete ENI: %v", err)
+	}
+	log.Println("Deleted ENI")
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
